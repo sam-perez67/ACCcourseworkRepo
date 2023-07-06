3b48b57429f30543aeda6e4c745578503e88ccf3
sam-perez67 <sam.perez@g.austincc.edu>
2023-07-06 16:02:39 -0500

    Modified sam_perez_lab4c.py to include a function that will return the letter grade of a student

--- sam_perez_lab4c.py
+++ sam_perez_lab4c.py
@@ -2,7 +2,18 @@ test_score = float(input('Enter your test score, if there is no test score, ente
 total = 0
 count = 0
 while test_score != -1:
-    print(test_score)
+    if test_score < 0 or test_score > 100:
+        print('Invalid test score, please enter a valid test score')
+    elif test_score >= 90:
+        print(test_score,'is an A')
+    elif test_score >= 80:
+        print(test_score, 'is a B')
+    elif test_score >= 70:
+        print(test_score, 'is a C')
+    elif test_score >= 60:
+        print(test_score, 'is a D')
+    else:
+        print(test_score, 'is an F')
     total += test_score
     count += 1
     test_score = float(input('Enter your test score, if there is no test score, enter -1: '))
